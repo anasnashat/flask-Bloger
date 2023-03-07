4673aa6f08f95bd1bbab5695be865ee02792e89d
anas nashat <72121781+anasnashat@users.noreply.github.com>
2023-03-07 02:39:12 +0200

    Add register system

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash, session, g
 import sqlite3
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+app.secret_key = "e6c0b139e0338a77479dccd2d499f38bea68ec238b3fdaf71d1bb098d8df2a29"
 
 DATABASE = 'database.db'
 
@@ -41,6 +44,47 @@ def create_post():
         return redirect(url_for('index'))
 
     return render_template('create.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+
+        error = None
+        if not name:
+            error = 'الاسم مطلوب'
+        elif not email:
+            error = 'البريد الإلكتروني مطلوب'
+        elif not username:
+            error = 'اسم المستخدم مطلوب'
+        elif not password:
+            error = 'كلمة المرور مطلوبة'
+        elif not password2:
+            error = 'تأكيد كلمة المرور مطلوب'
+        elif password != password2:
+            error = 'كلمة المرور غير متطابقة'
+        if error is None:
+            connection = get_db()
+            try:
+                user = connection.execute('INSERT INTO users (username,name,email,password) VALUES (?,?,?,?)',
+                                          (username, name, email, generate_password_hash(password)))
+                connection.commit()
+                connection.close()
+            except sqlite3.IntegrityError:
+                error = f"المستخدم {username} موجود بالفعل"
+            else:
+                return redirect(url_for('login'))
+
+        flash(error)
+
+    if request.method == "GET":
+        return render_template('auth/register.html')
+
 
 
 if __name__ == '__main__':
