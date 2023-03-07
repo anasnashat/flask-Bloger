6471b7e1aff00ca0053444c38025c4bca4fb7cce
anas nashat <72121781+anasnashat@users.noreply.github.com>
2023-03-07 17:42:58 +0200

    add update and Delete and make code more clean . finished the project started in 6/3/2023 to 7/3/2023

--- blog.py
+++ blog.py
@@ -1,16 +1,28 @@
 import functools
 import sqlite3
 
-from flask import request, render_template, redirect, url_for, Blueprint, g
+from flask import request, render_template, redirect, url_for, Blueprint, g, abort, flash
 
 DATABASE = 'database.db'
 
 bp = Blueprint('bp', __name__, url_prefix='/posts')
 
+
 def get_db():
     db = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
     return db
+
+
+def get_post(post_id , check_author = True):
+    post = get_db().execute('SELECT * FROM posts WHERE id=?', (post_id,)).fetchone()
+    if post is None:
+        abort(404, f'can not found this post with id {post_id}')
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
 
 def login_required(func):
     @functools.wraps(func)
@@ -34,8 +46,7 @@ def index():
 
 @bp.route('/<int:post_id>')
 def show_post(post_id):
-    connection = get_db()
-    post = connection.execute(f"SELECT * FROM posts WHERE id=?", (post_id,)).fetchone()
+    post = get_post(post_id, check_author=False)
 
     return render_template('show.html', post=post)
 
@@ -47,9 +58,44 @@ def create_post():
         connection = get_db()
         title = request.form['title']
         body = request.form['body']
-        post = connection.execute('INSERT INTO posts (title,body,author_id) VALUES (?,?,?)', (title, body, 1))
+        post = connection.execute('INSERT INTO posts (title,body,author_id) VALUES (?,?,?)', (title, body, g.user.id))
         connection.commit()
         connection.close()
         return redirect(url_for('bp.index'))
 
     return render_template('create.html')
+
+
+@bp.route("/<int:post_id>/update", methods=["GET", "POST"])
+def update_post(post_id):
+    post = get_post(post_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'the title is required'
+        if not body:
+            error = 'the body is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute('UPDATE posts SET title = ? , body = ? WHERE id = ?', (title, body, post_id))
+            db.commit()
+            db.close()
+            return redirect(url_for('bp.index'))
+
+    return render_template('create.html', post=post)
+
+
+@bp.route('/<int:post_id>/delete' , methods=['POST'])
+def delete_post(post_id):
+    post = get_post(post_id)
+    db = get_db()
+    db.execute('DELETE FROM posts WHERE id=?',(post_id,)).fetchone()
+    db.commit()
+    db.close()
+    return redirect(url_for('bp.index'))
